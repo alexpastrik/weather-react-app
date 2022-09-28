bf6c25d8ad4a988318345ec6b66ceed6244455b8
alexpastrik <102768664+alexpastrik@users.noreply.github.com>
2022-09-28 13:22:42 -0400

    axios installed and working

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Weather() {
-  return <h2>Hello</h2>;
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [temperature, setTemperature] = useState(null);
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setTemperature(response.data.main.temp);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e29b8864f34959087569c0fc55a5191a&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        {Math.round(temperature)}
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
